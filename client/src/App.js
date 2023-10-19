c57a80efbf1801b9b9c7f9a83d8900b21ab12b59
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-10-20 02:59:48 +0800

    added react-toastify toast

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import Home from './resources/js/Pages/Booking/Home'
 import Register from './resources/js/Pages/Auth/Register'
 import Login from './resources/js/Pages/Auth/Login'
+import Dashboard from './resources/js/Pages/Auth/Dashboard'
+
 import TopNav from "./resources/js/Components/TopNav";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <BrowserRouter>
     <TopNav/>
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable
+      pauseOnHover={false}
+      theme="dark"
+      />
     <Switch>
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/login" component={Login}></Route>
       <Route exact path="/register" component={Register}></Route>
+      <Route exact path="/dashboard" component={Dashboard}></Route>
     </Switch>
     </BrowserRouter>
   );
